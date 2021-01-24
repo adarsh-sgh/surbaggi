ff2d02468c34bc58c3cb30d9d4fd1bf85d8c7a58
Lakki <thakurluckysinghbrh@gmail.com>
2021-01-24 08:53:09 +0530

    added notice div

--- public/surbaggi.js
+++ public/surbaggi.js
@@ -330,3 +330,33 @@ function pp(coordinateString,value=undefined) {
       pp(`${midPointx}${midPointy}`,3)
     }
   }
+
+  //style.js
+  const overlay = document.querySelector(".overlay");
+nt = document.getElementById("notice");
+let timeout;
+function notice(message, time = false) {
+  overlay.style.display = "flex";
+  nt.innerHTML = message;
+  if (time) {
+    if (typeof timeout) {
+      clearTimeout(timeout);
+    }
+    setTimeout(() => {
+      fade();
+    }, time);
+  }
+}
+function dismiss() {
+  overlay.style.display = "none";
+}
+function fade(selector = ".overlay") {
+  element = document.querySelector(selector);
+  if (element.style.opacity && element.style.opacity < 0.1) {
+    element.style.display = "none";
+    element.style.opacity = 1;
+    return;
+  }
+  element.style.opacity = (element.style.opacity || 1) / 1.08;
+  setTimeout(() => fade(selector), 20);
+}
